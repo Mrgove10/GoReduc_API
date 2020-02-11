e1fb4d0c91d9ca5fd96214f02799c44b5e32f3bc
Adrien RICHARD <richardadrien0@gmail.com>
2020-02-11 11:07:46 +0100

    fixed get result

--- GetReducs.js
+++ GetReducs.js
@@ -1,17 +1,29 @@
 'use strict';
 
-var AWS = require('aws-sdk'),
-    documentClient = new AWS.DynamoDB.DocumentClient();
+var AWS = require('aws-sdk');
+var documentClient = new AWS.DynamoDB.DocumentClient();
 
-exports.readall = function (event, context, callback) {
-    var params = {
-        TableName: process.env.TABLE_NAME
-    };
-    documentClient.scan(params, function (err, data) {
-        if (err) {
-            callback(err, null);
-        } else {
-            callback(null, data.Items);
-        }
-    });
+exports.handler = function (event, context, callback) {
+    var response = {
+        "isBase64Encoded": false,
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        },
+        "body": JSON.stringify(event['pathParameters'])
+    }
+    callback(null, response);
+
+    //return event['pathParameters'];
+    /* var params = {
+          TableName: process.env.TABLE_NAME
+      };
+      documentClient.scan(params, function (err, data) {
+          if (err) {
+              callback(err, null);
+          } else {
+              callback(null, data.Items);
+          }
+      });*/
+
 }
