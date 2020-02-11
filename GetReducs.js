634a72cd50c8bd864f4c35eb7898688b24a129ef
Adrien RICHARD <richardadrien0@gmail.com>
2020-02-11 11:54:56 +0100

    addedstuff

--- GetReducs.js
+++ GetReducs.js
@@ -4,26 +4,28 @@ var AWS = require('aws-sdk');
 var documentClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
-    var response = {
-        "isBase64Encoded": false,
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-        },
-        "body": JSON.stringify(event['pathParameters'])
-    }
-    callback(null, response);
 
-    //return event['pathParameters'];
-    /* var params = {
-          TableName: process.env.TABLE_NAME
-      };
-      documentClient.scan(params, function (err, data) {
-          if (err) {
-              callback(err, null);
-          } else {
-              callback(null, data.Items);
-          }
-      });*/
+    var params = {
+        TableName: process.env.TABLE_NAME,
+        Key: {
+            "ID": event['pathParameters']["id"],
+        }
+    };
 
+    documentClient.get(params, function (err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2)); callback(err, null);
+        } else {
+            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            var response = {
+                "isBase64Encoded": false,
+                "statusCode": 200,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+                },
+                "body": JSON.stringify(data)
+            }
+            callback(null, response);
+        }
+    });
 }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # GoReduc_API
+
 AWS Api
 
-https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+<https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property>
 
-
-aws lambda update-function-code --function-name CreateReduc  --zip-file .\createreduc.zip
+<https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.NodeJs.03.html>
